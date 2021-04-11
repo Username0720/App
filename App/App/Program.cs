6a207d77e4016e564bfde7b09e1b4401bd17576d
unknown <WalleyWay@gmail.com>
2021-04-11 22:42:13 +0300

    first commit

---
+++ App/App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var close = "";
+            while (true)
+            {
+                var configuration = new ConfigurationOptions();
+                configuration.EndPoints.Add("localhost", 6379);
+                var redis = ConnectionMultiplexer.Connect(configuration);
+                var db = redis.GetDatabase(7);
+                const string key = "firstnumber";
+                const string key1 = "secondnumber";
+                db.KeyDelete(key);
+                db.KeyDelete(key1);
+                int n1 = 10;
+                int n2 = 1;
+                try
+                {
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    n2 = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+                db.StringSet(key, n1);
+                db.StringSet(key1, n2);
+                Console.WriteLine("Введите 'exit', чтобы закрыть приложение");
+                close = Console.ReadLine();
+                if (close == "exit") break;
+            }
+        }
+    }
+}
---
+++ App1/App1/Program.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var close = "";
+            while (true)
+            {
+                var configuration = new ConfigurationOptions();
+                configuration.EndPoints.Add("localhost", 6379);
+                var redis = ConnectionMultiplexer.Connect(configuration);
+                var db = redis.GetDatabase(7);
+                RedisChannel redisChannel = "warnings";
+                const string key = "firstnumber";
+                const string key1 = "secondnumber";
+                if (db.KeyExists(key) && db.KeyExists(key1))
+                {
+                    int firstnumber = Convert.ToInt32(db.StringGet(key));
+                    int secondnumber = Convert.ToInt32(db.StringGet(key1));
+                    int result = firstnumber * secondnumber;
+                    Console.WriteLine(result);
+                }
+                else Console.WriteLine("ключи не существуют");
+                Console.WriteLine("Введите 'exit', чтобы закрыть приложение");
+                close = Console.ReadLine();
+                if (close == "exit") break;
+            }
+        }
+    }
+}
